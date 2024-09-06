6d8405ca84687e0246856dd990d344fb9786290f
Ansh Sharma <147230122+ansh-sx@users.noreply.github.com>
2024-09-06 15:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,11 +47,7 @@ DEFAULT_SEARCH_ENGINE_TIMEOUT = 5
 # behave differently, and we haven't tuned the prompt to make it optimal - this
 # is left to you, application creators, as an open problem.
 _rag_query_text = """
-{{You are a large language AI assistant built by Ansh Sharma. You are given a user question, and please write a clean, concise and accurate answer to the question. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context and cite the context at the end of each sentence if applicable.
-
- Your answer must be correct, accurate, and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context does not provide sufficient information.
-
- You are an AI search engine and a helpful AI agent. You have to create a researched professional answer like this I'm creating. Here are the examples:
+You are an AI search engine and a helpful AI agent. You have to create a researched professional answer like this I'm creating. Here are the examples:
 
  The response format: 
 
@@ -105,13 +101,12 @@ _rag_query_text = """
 }}
 
 This is a important note remember each keyword should have 1 content line and a emoji...keyword count == content lines == emoji count..and remeber 1 content line means a line written in double quotes ...and return all data in json format ..dont write anything else 
-Please cite the contexts with the reference numbers, in the format [citation:x]. If a sentence comes from multiple contexts, please list all applicable citations, like [citation:3][citation:5]. Other than code and specific names and citations, your answer must be written in the same language as the question. If there are too many citations, choose the best of them.
 Dont write anything except this json form, it should not seem to user that this is a ai generated response ...
 Here are the set of contexts:
 
 {{context}}
 
-Remember, don't blindly repeat the contexts. And here is the user question:
+here is the user question:
 """
 
 
@@ -126,19 +121,19 @@ stop_words = [
     "End.",
 ]
 
-_more_questions_prompt = """
-You are a helpful assistant that helps the user to ask related questions, based on user's original question and the related contexts. Please identify worthwhile topics that can be follow-ups, and write questions no longer than 20 words each. Please make sure that specifics, like events, names, locations, are included in follow up questions so they can be asked standalone. For example, if the original question asks about "the Manhattan project", in the follow up question, do not just say "the project", but use the full name "the Manhattan project". The format of giving the responses and generating the questions shoudld be like this:
+# _more_questions_prompt = """
+# You are a helpful assistant that helps the user to ask related questions, based on user's original question and the related contexts. Please identify worthwhile topics that can be follow-ups, and write questions no longer than 20 words each. Please make sure that specifics, like events, names, locations, are included in follow up questions so they can be asked standalone. For example, if the original question asks about "the Manhattan project", in the follow up question, do not just say "the project", but use the full name "the Manhattan project". The format of giving the responses and generating the questions shoudld be like this:
+# 
+# 1. [Question 1]
+# 2. [Question 2] 
+# 3. [Question 3]
+#
+# Here are the contexts of the question:
 
-1. [Question 1]
-2. [Question 2] 
-3. [Question 3]
+# {context}
 
-Here are the contexts of the question:
-
-{context}
-
-Remember, based on the original question and related contexts, suggest three such further questions. Do NOT repeat the original question. Each related question should be no longer than 20 words. Here is the original question:
-"""
+# Remember, based on the original question and related contexts, suggest three such further questions. Do NOT repeat the original question. Each related question should be no longer than 20 words. Here is the original question:
+# """
 
 
 def search_with_serper(query: str, subscription_key=SERPER_API, prints=False):
